2c75441bca07c5722280279d2a1d97d84ac14fbe
wuxiao <wuz1xiao@163.com>
2021-11-10 16:36:51 +0800

    first commit

---
+++ src/main/java/com/java/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.java.git;
+
+/**
+ * @author HH
+ * @date 2021-11-10 16:30
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
